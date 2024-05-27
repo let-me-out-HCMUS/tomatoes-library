2df3bfa083c9c3462509bbb7fb83700fbe47885c
Spartan - Phuc Nguyen <phuc.nguyen@c0x12c.com>
2024-05-27 23:13:51 +0700

    navbar, layout, footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReadingPage from "./shared/pages/reading-page";
-import Home from "./features/Home/Home";
-function App() {
+import Home from "./shared/pages/Home";
+import AppLayout from "./common/AppLayout";
+
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="stories/:slug/">
-          <Route path=":chapter" element={<ReadingPage />}></Route>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="stories/:slug/">
+            <Route path=":chapter" element={<ReadingPage />}></Route>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
