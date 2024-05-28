78ef9d15d8e626d7a171b9f256f63b6fc1ad4cfb
hkhangus <huukhangtc@gmail.com>
2024-05-28 19:52:52 +0700

    [story-page]: handle not found

--- client/src/shared/pages/Story.jsx
+++ client/src/shared/pages/Story.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import Story from '../../features/StoryPage/components/Story';
-import CircularProgress from '@mui/material/CircularProgress';
-import { useParams } from 'react-router-dom'
-import { getStory } from '../../api/story';
+import React from "react";
+import Story from "../../features/StoryPage/components/Story";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useParams } from "react-router-dom";
+import { getStory } from "../../api/story";
 
 const StoryPage = () => {
-  const { slug } = useParams()
-  
-  const [story, setStory] = React.useState(null)
+  const { slug } = useParams();
+
+  const [story, setStory] = React.useState(null);
 
   React.useEffect(() => {
     const getData = async () => {
-      const res = await getStory(slug)
-        console.log(res)
-      
-      if (!res.status === 'fail') {
-        return window.location.replace('/404')
+      const res = await getStory(slug);
+      console.log(res);
+
+      if (!res.status === "fail") {
+        return window.location.replace("/404");
       }
 
-      setStory(res.data)
-    }
+      setStory(res.data);
+    };
 
-    getData()
-  }, [slug])
+    getData();
+  }, [slug]);
 
-  if (!story) return <CircularProgress />
+  if (!story) return <div className=" text-center text-6xl p-10">Page not found</div>;
 
   return (
-    <div className='py-10'>
+    <div className="py-10">
       <Story storyData={story} />
     </div>
-  )
-}
+  );
+};
 
-export default StoryPage
+export default StoryPage;
