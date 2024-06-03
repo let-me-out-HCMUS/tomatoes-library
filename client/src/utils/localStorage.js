ef5ca75ef9fd93df67de68b80c47ae2ad199121e
hkhangus <huukhangtc@gmail.com>
2024-06-03 23:22:44 +0700

    refactor feat which interact with localstorage

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,8 +1,17 @@
 export const getReadChap = (name) => {
-    return JSON.parse(localStorage.getItem(name)) || []; 
-}
+  return JSON.parse(localStorage.getItem(name)) || [];
+};
 
 export const getContinueChap = (name) => {
-    let list = JSON.parse(localStorage.getItem(name)) || [];
-    return list[list.length - 1] || 1;
-}
+  let list = JSON.parse(localStorage.getItem(name)) || [];
+  return list[list.length - 1] || 1;
+};
+
+export const addChapToLocalStorage = (slug, chapter) => {
+  let list = JSON.parse(localStorage.getItem(slug)) || [];
+  if (!list.includes(chapter)) {
+    list.push(chapter);
+    list.sort((a, b) => a - b);
+    localStorage.setItem(`${slug}`, JSON.stringify(list));
+  }
+};
