d9332c1745b18880f7230f2ca47a2bfce2001964
hkhangus <huukhangtc@gmail.com>
2024-06-03 22:27:26 +0700

    update config axios

--- client/src/api/axios-instance.js
+++ client/src/api/axios-instance.js
@@ -9,6 +9,20 @@ export const axiosClient = axios.create({
     },
 });
 
+axiosClient.defaults.timeout = 7000; // 7 seconds
+
+// Add a request interceptor
+axiosClient.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
 // Add a response interceptor
 axiosClient.interceptors.response.use(
     function (response) {
--- client/src/api/story.js
+++ client/src/api/story.js
@@ -1,28 +1,36 @@
 import { axiosClient } from "./axios-instance";
 
 export const getStories = async () => {
-    try {
-        const response = await axiosClient.get('/stories');
-        return response;
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    const response = await axiosClient.get("/stories");
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const getStory = async (name) => {
-    try {
-        const response = await axiosClient.get(`/stories/${name}`);
-        return response;
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    const response = await axiosClient.get(`/stories/${name}`);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const getChapter = async (name, chapter, server) => {
-    try {
-        const response = await axiosClient.get(`/stories/${name}/content/${chapter}?source=${server}`);
-        return response;
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    const response = await axiosClient.get(
+      `/stories/${name}/content/${chapter}`,
+      {
+        timeout: 10000, // Wait for 7 seconds
+        params: {
+          source: server,
+        },
+      }
+    );
+    return response
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+};
