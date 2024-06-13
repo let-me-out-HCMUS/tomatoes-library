e8ba44151adf8c1ad82f3a1ca39fa8576a89916e
hkhangus <huukhangtc@gmail.com>
2024-06-13 21:44:10 +0700

    fix linh tinh

--- client/src/shared/pages/SearchPage.jsx
+++ client/src/shared/pages/SearchPage.jsx
@@ -1,27 +1,32 @@
 import CategorySide from '../../features/SearchPage/CategorySide';
 import SearchContent from '../../features/SearchPage/SearchContent';
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { getCategories } from '../../api/category';
 import slugConverter from '../../utils/slugConverter';
 import { getStoriesByCategory } from '../../api/category';
 import { searchStories } from '../../api/story';
+import { SourceOrderContext } from '../../shared/context/SourceOrderContext';
+import Spinner from "../../features/common/Spinner";
 
 function SearchPage({ isSearch = false }) {
   const { slug } = useParams();
+  const { getSourceOrder } = useContext(SourceOrderContext);
+  const sourceOrder = getSourceOrder();
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState();
   const [stories, setStories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   // Use Spinner wait for data
-
+  console.log(sourceOrder)
   // Categories
   useEffect(() => {
     async function fetchData() {
       try {
         let res = await getCategories();
         setCategories(res.data);
-        console.log(res);
-        console.log(categories);
+        // console.log(res);
+        // console.log(categories);
         if (!isSearch) {
           setSelectedCategory(
             res.data.filter((category) => slugConverter(category) === slug)
@@ -33,7 +38,7 @@ function SearchPage({ isSearch = false }) {
     }
     fetchData();
   }, []);
-
+  console.log(isSearch  )
   // Stories
   useEffect(() => {
     if (slug) {
@@ -49,16 +54,19 @@ function SearchPage({ isSearch = false }) {
         }
         // Seacrh
         else {
-          console.log(slug);
-          const res = await searchStories(slug);
+          console.log('source',sourceOrder[0].source);
+          const res = await searchStories(slug, sourceOrder[0].source);
           setStories(res.data);
           console.log(res);
         }
+        setIsLoading(false);
       };
       fetchData();
+      
     }
   }, [slug]);
 
+  if (isLoading) return Spinner();
   return (
     <div className=" flex lg:my-28 lg:mx-16 ">
       <CategorySide category={categories} selectedCategory={selectedCategory} />
