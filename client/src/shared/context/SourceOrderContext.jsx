ddbe98980a51c488e709b5bb6db6ad9dba7cbe49
cukhoaimon <phucmapcaumieu@gmail.com>
2024-06-14 21:17:43 +0700

    code chuan

--- client/src/shared/context/SourceOrderContext.jsx
+++ client/src/shared/context/SourceOrderContext.jsx
@@ -1,71 +1,65 @@
-import { createContext, useState, useEffect } from "react";
-import { getSources } from "../../api/source";
+import {createContext, useEffect, useState} from "react";
+import {getSources} from "../../api/source";
 
 export const SourceOrderContext = createContext();
 
 const SOURCES_ORDER = 'say-gex-sources-order'
 
-export default function ({ children }){
-    const [sourceOrder, setSourceOrder] = useState([]); // [{}]
+export default function ({children}) {
+	const [sourceOrder, setSourceOrder] = useState([]); // [{}]
 
-    useEffect(() => {
-        (async function() {
-            const response = await getSources()
-            if (localStorage[SOURCES_ORDER]){
-                if (response.status === "success") {
-                    let data = response.data
-                    if (data.length !== JSON.parse(localStorage[SOURCES_ORDER]).length) {
-                        localStorage.setItem(SOURCES_ORDER, JSON.stringify(data))
-                    }
-                }
-                setSourceOrder(JSON.parse(localStorage[SOURCES_ORDER]))
-                return
-            }
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await getSources()
+				const fromServer = response.data;
+				const fromLocal = JSON.parse(localStorage.getItem(SOURCES_ORDER));
 
-            try {
-                const response = await getSources()
-                if (response.status === "success") {
-                    let data = response.data
-                    localStorage.setItem(SOURCES_ORDER, JSON.stringify(data))
-                    setSourceOrder(data)
-                }
-            } catch(error) {
-                console.log(error)
-            }
-        })()
-    }, [])
+				if (fromServer.length !== fromLocal.length) {
+					localStorage.setItem(SOURCES_ORDER, JSON.stringify(fromLocal));
+					setSourceOrder(fromServer)
+				} else {
+					setSourceOrder(fromLocal);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		}
 
-    const getSourceOrder = () => {
-        return sourceOrder;
-    };
+		fetchData().then(r => console.log(r))
+	}, [])
 
-    const moveSourceUp = (index) => {
-        if (index === 0) return; // First source can't move up
-    
-        const newSourceOrder = [...sourceOrder];
-        [newSourceOrder[index - 1], newSourceOrder[index]] = [newSourceOrder[index], newSourceOrder[index - 1]];
-        setSourceOrder(newSourceOrder);
-    };
+	const getSourceOrder = () => {
+		return sourceOrder;
+	};
 
-    const moveSourceDown = (index) => {
-        if (index === sourceOrder.length - 1) return; // Last source can't move down
-    
-        const newSourceOrder = [...sourceOrder];
-        [newSourceOrder[index], newSourceOrder[index + 1]] = [newSourceOrder[index + 1], newSourceOrder[index]];
-        setSourceOrder(newSourceOrder);
-    };
+	const moveSourceUp = (index) => {
+		if (index === 0) return; // First source can't move up
 
-    const submit = () => {
-        localStorage.setItem(SOURCES_ORDER, JSON.stringify(sourceOrder))
-        window.location.reload()
-    }
+		const newSourceOrder = [...sourceOrder];
+		[newSourceOrder[index - 1], newSourceOrder[index]] = [newSourceOrder[index], newSourceOrder[index - 1]];
+		setSourceOrder(newSourceOrder);
+	};
 
-    const value = {
-        getSourceOrder,
-        moveSourceUp,
-        moveSourceDown,
-        submit
-    };
+	const moveSourceDown = (index) => {
+		if (index === sourceOrder.length - 1) return; // Last source can't move down
 
-    return <SourceOrderContext.Provider value={value}>{children}</SourceOrderContext.Provider>;
+		const newSourceOrder = [...sourceOrder];
+		[newSourceOrder[index], newSourceOrder[index + 1]] = [newSourceOrder[index + 1], newSourceOrder[index]];
+		setSourceOrder(newSourceOrder);
+	};
+
+	const submit = () => {
+		localStorage.setItem(SOURCES_ORDER, JSON.stringify(sourceOrder))
+		window.location.reload()
+	}
+
+	const value = {
+		getSourceOrder,
+		moveSourceUp,
+		moveSourceDown,
+		submit
+	};
+
+	return <SourceOrderContext.Provider value={value}>{children}</SourceOrderContext.Provider>;
 };
--- server/config/loadConfig.js
+++ server/config/loadConfig.js
@@ -1,228 +1,227 @@
 const fs = require("fs");
 const fetch = require("node-fetch");
-const { JSDOM } = require("jsdom");
+const {JSDOM} = require("jsdom");
 
 exports.getStoryContent = async function (source, storySlug, chapter) {
-  try {
-    const data = fs.readFileSync("./config/config.json", {
-      encoding: "utf-8",
-    });
+	try {
+		const data = fs.readFileSync("./config/config.json", {
+			encoding: "utf-8",
+		});
 
-    const config = JSON.parse(data);
-    let sourceConfig = config.find((cfg) => cfg.source === source);
-    if (!sourceConfig) {
-      sourceConfig = config[0];
-    }
+		const config = JSON.parse(data);
+		let sourceConfig = config.find((cfg) => cfg.source === source);
+		if (!sourceConfig) {
+			sourceConfig = config[0];
+		}
 
-    const params = sourceConfig.getContent.params.join(", ");
+		const params = sourceConfig.getContent.params.join(", ");
 
-    const getContentFunc = new Function(
-      "fetch",
-      "JSDOM",
-      `return async function(${params}){
+		const getContentFunc = new Function(
+			"fetch",
+			"JSDOM",
+			`return async function(${params}){
                 ${sourceConfig.getContent.handler}
             }`
-    )(fetch, JSDOM);
+		)(fetch, JSDOM);
 
-    const result = await getContentFunc(storySlug, chapter);
-    return result;
-  } catch (error) {
-    console.error("Error loading config:", error);
-    return "";
-  }
+		return await getContentFunc(storySlug, chapter);
+	} catch (error) {
+		console.error("Error loading config:", error);
+		return "";
+	}
 };
 
 exports.listStories = async function (order) {
-  try {
-    const data = fs.readFileSync("./config/config.json", {
-      encoding: "utf-8",
-    });
+	try {
+		const data = fs.readFileSync("./config/config.json", {
+			encoding: "utf-8",
+		});
 
-    const config = JSON.parse(data);
-    const totalStories = [];
-    for (let i = 0; i < order.length; i++) {
-      try {
-        const sourceConfig = config.find(cfg => cfg.source === order[i]);
+		const config = JSON.parse(data);
+		const totalStories = [];
+		for (let i = 0; i < order.length; i++) {
+			try {
+				const sourceConfig = config.find(cfg => cfg.source === order[i]);
 
-        const listStoriesFunc = new Function(
-          "fetch",
-          "JSDOM",
-          `return async function(){
+				const listStoriesFunc = new Function(
+					"fetch",
+					"JSDOM",
+					`return async function(){
                     ${sourceConfig.listStories.handler}
                 }`
-        )(fetch, JSDOM);
+				)(fetch, JSDOM);
 
-        const result = await listStoriesFunc();
-        for (let j = 0; j < result.length; j++) {
-          const idx = totalStories.findIndex(
-            (story) => story.name === result[j].name
-          );
-          if (idx === -1) {
-            totalStories.push(result[j]);
-          } else {
-            totalStories[idx].totalChapter = Math.max(
-              totalStories[idx].totalChapter,
-              result[j].totalChapter
-            );
-          }
-        }
-      } catch (err) {
-        console.error("Error at ", order[i], err);
-      }
-    }
+				const result = await listStoriesFunc();
+				for (let j = 0; j < result.length; j++) {
+					const idx = totalStories.findIndex(
+						(story) => story.name === result[j].name
+					);
+					if (idx === -1) {
+						totalStories.push(result[j]);
+					} else {
+						totalStories[idx].totalChapter = Math.max(
+							totalStories[idx].totalChapter,
+							result[j].totalChapter
+						);
+					}
+				}
+			} catch (err) {
+				console.error("Error at ", order[i], err);
+			}
+		}
 
-    return totalStories;
-  } catch (error) {
-    console.error("Error loading config:", error);
-    return [];
-  }
+		return totalStories;
+	} catch (error) {
+		console.error("Error loading config:", error);
+		return [];
+	}
 };
 
 exports.getStory = async function (storySlug) {
-  try {
-    const data = fs.readFileSync("./config/config.json", {
-      encoding: "utf-8",
-    });
+	try {
+		const data = fs.readFileSync("./config/config.json", {
+			encoding: "utf-8",
+		});
 
-    const config = JSON.parse(data);
-    const totalSource = [];
-    let returnStory = {};
-    for (let i = 0; i < config.length; i++) {
-      sourceConfig = config[i];
+		const config = JSON.parse(data);
+		const totalSource = [];
+		let returnStory = {};
+		for (let i = 0; i < config.length; i++) {
+			sourceConfig = config[i];
 
-      const params = sourceConfig.getStory.params.join(", ");
+			const params = sourceConfig.getStory.params.join(", ");
 
-      try {
-        const getStoryFunc = new Function(
-          "fetch",
-          "JSDOM",
-          `return async function(${params}){
+			try {
+				const getStoryFunc = new Function(
+					"fetch",
+					"JSDOM",
+					`return async function(${params}){
         ${sourceConfig.getStory.handler}
         }`
-        )(fetch, JSDOM);
+				)(fetch, JSDOM);
 
-        const storyDetail = await getStoryFunc(storySlug);
-        if (storyDetail != null) {
-          returnStory = storyDetail;
-          totalSource.push(sourceConfig.source);
-        }
-      } catch (err) {
-        console.log("Cannot get data of source: ", sourceConfig.source, err)
-      }
-    }
+				const storyDetail = await getStoryFunc(storySlug);
+				if (storyDetail != null) {
+					returnStory = storyDetail;
+					totalSource.push(sourceConfig.source);
+				}
+			} catch (err) {
+				console.log("Cannot get data of source: ", sourceConfig.source, err)
+			}
+		}
 
-    returnStory.source = totalSource;
-    return returnStory;
-  } catch (error) {
-    console.error("Error loading config:", error);
-    return {};
-  }
+		returnStory.source = totalSource;
+		return returnStory;
+	} catch (error) {
+		console.error("Error loading config:", error);
+		return {};
+	}
 };
 
 exports.search = async function (source, searchString) {
-  try {
-    const data = fs.readFileSync("./config/config.json", {
-      encoding: "utf-8",
-    });
+	try {
+		const data = fs.readFileSync("./config/config.json", {
+			encoding: "utf-8",
+		});
 
-    const config = JSON.parse(data);
-    let sourceConfig = config.find((cfg) => cfg.source === source);
-    if (!sourceConfig) {
-      sourceConfig = config[0];
-    }
+		const config = JSON.parse(data);
+		let sourceConfig = config.find((cfg) => cfg.source === source);
+		if (!sourceConfig) {
+			sourceConfig = config[0];
+		}
 
-    const params = sourceConfig.search.params.join(", ");
+		const params = sourceConfig.search.params.join(", ");
 
-    const searchFunc = new Function(
-      "fetch",
-      "JSDOM",
-      `return async function(${params}){
+		const searchFunc = new Function(
+			"fetch",
+			"JSDOM",
+			`return async function(${params}){
                 ${sourceConfig.search.handler}
             }`
-    )(fetch, JSDOM);
+		)(fetch, JSDOM);
 
-    const result = await searchFunc(searchString);
-    return result;
-  } catch (error) {
-    console.error("Error loading config:", error);
-    return [];
-  }
+		const result = await searchFunc(searchString);
+		return result;
+	} catch (error) {
+		console.error("Error loading config:", error);
+		return [];
+	}
 };
 
 exports.listCategories = async function (source) {
-  try {
-    const data = fs.readFileSync("./config/config.json", {
-      encoding: "utf-8",
-    });
+	try {
+		const data = fs.readFileSync("./config/config.json", {
+			encoding: "utf-8",
+		});
 
-    const config = JSON.parse(data);
-    let sourceConfig = config.find((cfg) => cfg.source === source);
-    if (!sourceConfig) {
-      sourceConfig = config[0];
-    }
+		const config = JSON.parse(data);
+		let sourceConfig = config.find((cfg) => cfg.source === source);
+		if (!sourceConfig) {
+			sourceConfig = config[0];
+		}
 
-    const listCategoriesFunc = new Function(
-      "fetch",
-      "JSDOM",
-      `return async function(){
+		const listCategoriesFunc = new Function(
+			"fetch",
+			"JSDOM",
+			`return async function(){
                 ${sourceConfig.category.list.handler}
             }`
-    )(fetch, JSDOM);
+		)(fetch, JSDOM);
 
-    const result = await listCategoriesFunc();
-    return result;
-  } catch (error) {
-    console.error("Error loading config:", error);
-    return [];
-  }
+		const result = await listCategoriesFunc();
+		return result;
+	} catch (error) {
+		console.error("Error loading config:", error);
+		return [];
+	}
 };
 
 exports.listStoriesByCategory = async function (source, categorySlug) {
-  try {
-    const data = fs.readFileSync("./config/config.json", {
-      encoding: "utf-8",
-    });
+	try {
+		const data = fs.readFileSync("./config/config.json", {
+			encoding: "utf-8",
+		});
 
-    const config = JSON.parse(data);
-    let sourceConfig = config.find((cfg) => cfg.source === source);
-    if (!sourceConfig) {
-      sourceConfig = config[0];
-    }
+		const config = JSON.parse(data);
+		let sourceConfig = config.find((cfg) => cfg.source === source);
+		if (!sourceConfig) {
+			sourceConfig = config[0];
+		}
 
-    const params = sourceConfig.category.getStories.params.join(", ");
+		const params = sourceConfig.category.getStories.params.join(", ");
 
-    const listCategoriesFunc = new Function(
-      "fetch",
-      "JSDOM",
-      `return async function(${params}){
+		const listCategoriesFunc = new Function(
+			"fetch",
+			"JSDOM",
+			`return async function(${params}){
                 ${sourceConfig.category.getStories.handler}
             }`
-    )(fetch, JSDOM);
+		)(fetch, JSDOM);
 
-    const result = await listCategoriesFunc(categorySlug);
-    return result;
-  } catch (error) {
-    console.error("Error loading config:", error);
-    return [];
-  }
+		const result = await listCategoriesFunc(categorySlug);
+		return result;
+	} catch (error) {
+		console.error("Error loading config:", error);
+		return [];
+	}
 };
 
 exports.listSources = async function () {
-  try {
-    const data = fs.readFileSync("./config/config.json", {
-      encoding: "utf-8",
-    });
+	try {
+		const data = fs.readFileSync("./config/config.json", {
+			encoding: "utf-8",
+		});
 
-    const config = JSON.parse(data);
+		const config = JSON.parse(data);
 
-    return config.map(cfg => {
-      return {
-        source: cfg.source,
-        name: cfg.name
-      }
-    })
-    
-  } catch (error) {
-    console.error("Error list sources: ", error)
-  }
+		return config.map(cfg => {
+			return {
+				source: cfg.source,
+				name: cfg.name
+			}
+		})
+
+	} catch (error) {
+		console.error("Error list sources: ", error)
+	}
 }
